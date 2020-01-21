baec64722dfea478286f3baacc9519dc21a3bbce
iqramqra <masrooriqra@gmail.com>
2020-01-21 14:49:21 -0500

    seeds 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,19 +3,9 @@ require 'faker'
 10.times do
     Coffee.create(
         flavor: Faker::Coffee.blend_name,
-        price: Faker::Number.number(2),
+        price: Faker::Number.number(1..6),
         toppings: Faker::Dessert.flavor
     )
 end
 
-# coffee1 = Coffee.create ("Java", 2, "whipped cream") 
-
-# Faker::Coffee.blend_name #=> "Summer Solstice"
-
-# Faker::Coffee.origin #=> "Antigua, Guatemala"
-
-# Faker::Coffee.variety #=> "Pacas"
-
-# Faker::Coffee.notes #=> "balanced, silky, marzipan, orange-creamsicle, bergamot"
-
-# Faker::Coffee.intensifier #=> "quick"
+# coffee1 = Coffee.create ("Java", 2, "whipped cream")
