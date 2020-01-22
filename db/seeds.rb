80f1c132ce973efded91d4c6c978828bb557506f
iqramqra <masrooriqra@gmail.com>
2020-01-22 16:31:41 -0500

    updated seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,13 @@
 require 'faker'
 Order.destroy_all
 
-10.times do
-    Coffee.create(
-        flavor: Faker::Coffee.blend_name,
-        price: Faker::Number.between(from: 1, to: 10),
-        toppings: Faker::Dessert.flavor
-    )
-end
-
-# coffee1 = Coffee.create ("Java", 2, "whipped cream")
-
+cinnamon = Coffee.create(flavor: 'cinnamon', price: Faker::Number.between(from: 1, to: 10), toppings: Faker::Dessert.flavor)
+light = Coffee.create(flavor: 'light', price: Faker::Number.between(from: 1, to: 10), toppings: Faker::Dessert.flavor)
+medium = Coffee.create(flavor: 'medium', price: Faker::Number.between(from: 1, to: 10), toppings: Faker::Dessert.flavor)
+dark = Coffee.create(flavor: 'dark', price: Faker::Number.between(from: 1, to: 10), toppings: Faker::Dessert.flavor)
+french = Coffee.create(flavor: 'french', price: Faker::Number.between(from: 1, to: 10), toppings: Faker::Dessert.flavor)
+italian = Coffee.create(flavor: 'italian', price: Faker::Number.between(from: 1, to: 10), toppings: Faker::Dessert.flavor)
+ 
 5.times do 
     customer = Customer.create(
         name: Faker::Movies::HarryPotter.character
@@ -20,7 +17,3 @@ end
         coffee_id: Coffee.all.sample.id
     )
 end
-
-# 5.times do 
-    
-# end
